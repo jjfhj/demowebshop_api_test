e35b09d1cfff0ef7c793e246504b0cf01fa36663
Karina <aiwpront@gmail.com>
2021-12-30 00:48:11 +0400

    added App

---
+++ src/test/java/com/github/jjfhj/config/App.java
@@ -0,0 +1,7 @@
+package com.github.jjfhj.config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class App {
+    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
+}
