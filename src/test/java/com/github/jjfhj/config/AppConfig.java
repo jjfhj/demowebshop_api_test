1b3b4e184fc5ebc38d11a8559f3c38eea3005987
Karina <aiwpront@gmail.com>
2021-12-30 00:47:19 +0400

    added AppConfig

---
+++ src/test/java/com/github/jjfhj/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.github.jjfhj.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/app.properties"
+})
+public interface AppConfig extends Config {
+
+    @Key("webUrl")
+    String webUrl();
+
+    @Key("apiUrl")
+    String apiUrl();
+
+    @Key("userLogin")
+    String userLogin();
+
+    @Key("userPassword")
+    String userPassword();
+}
