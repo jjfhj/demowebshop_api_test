9a3b4052e91826568f22870651fb4f27909de311
Karina <aiwpront@gmail.com>
2022-01-07 01:16:02 +0400

    added test with Allure Listener and Custom Templates

--- src/test/java/com/github/jjfhj/tests/DemowebshopTest.java
+++ src/test/java/com/github/jjfhj/tests/DemowebshopTest.java
@@ -3,6 +3,7 @@ package com.github.jjfhj.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.jjfhj.config.App;
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,6 +15,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.github.jjfhj.filters.CustomLogFilter.customLogFilter;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 
@@ -51,11 +53,34 @@ public class DemowebshopTest {
     }
 
     @Test
-    @DisplayName("Отображение количества товара в корзине после его добавления через API")
-    void displayingNumberOfItemsInTheCartAfterAddingItViaAPITest() {
+    @DisplayName("Отображение количества товара в корзине после его добавления через API (Allure Listener)")
+    void displayingNumberOfItemsInTheCartAfterAddingItViaAPITestWithAllureTest() {
 
         step("Добавить товар 'Computing and Internet' в корзину", () ->
                 updateTopCartSection = given()
+                        .filter(new AllureRestAssured())
+                        .cookie("NOPCOMMERCE.AUTH", authorizationCookie)
+                        .when()
+                        .post("addproducttocart/details/13/1")
+                        .then().log().all()
+                        .statusCode(200)
+                        .extract()
+                        .path("updatetopcartsectionhtml"));
+
+        step("Открыть главную страницу магазина", () ->
+                open(""));
+
+        step("Количество товара = " + updateTopCartSection, () ->
+                $(".cart-qty").shouldHave(text(updateTopCartSection)));
+    }
+
+    @Test
+    @DisplayName("Отображение количества товара в корзине после его добавления через API (Custom Templates)")
+    void displayingNumberOfItemsInTheCartAfterAddingItViaAPITestWithTemplateTest() {
+
+        step("Добавить товар 'Computing and Internet' в корзину", () ->
+                updateTopCartSection = given()
+                        .filter(customLogFilter().withCustomTemplates())
                         .cookie("NOPCOMMERCE.AUTH", authorizationCookie)
                         .when()
                         .post("addproducttocart/details/13/1")
